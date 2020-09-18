eec8045bd40f8a7ac8faa6e905b2ef5fe2996d88
Peter Nigh <peternigh@Peters-MacBook-Pro.local>
2020-09-18 12:35:59 -0500

    MVP+stretch

--- challenges/arrays-callbacks.js
+++ challenges/arrays-callbacks.js
@@ -15,7 +15,7 @@ const zooAnimals = [
   { animal_name: "Australian pelican", population: 5, scientific_name: "Pelecanus conspicillatus", state: "West Virginia" },
 ];
 
-console.log(zooAnimals);
+// console.log(zooAnimals);
 
 /* Request 1: .forEach()
 
@@ -36,15 +36,18 @@ The zoos need a list of all their animal's names (animal_name only) converted to
 
 */
 
-// const lowCaseAnimalNames
-// console.log(lowCaseAnimalNames);
+
+
+const lowCaseAnimalNames = zooAnimals.map(lower => lower.animal_name.toLowerCase())
+
+console.log(lowCaseAnimalNames);
 
 /* Request 3: .filter() 
 
 The zoos are concerned about animals with a lower population count. Using filter, create a new array of objects called lowPopulationAnimals which contains only the animals with a population less than 5.
 
 */
-const lowPopulationAnimals
+const lowPopulationAnimals = zooAnimals.filter(pop => pop.population < 5)
 console.log(lowPopulationAnimals);
 
 /* Request 4: .reduce() 
@@ -52,7 +55,10 @@ console.log(lowPopulationAnimals);
 The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
 
 */
-let populationTotal = 0;
+let populationTotal = zooAnimals.reduce((param, item) => {
+  let sum = param + item.population;
+  return sum
+}, 0)
 console.log(populationTotal);
 
 
@@ -66,17 +72,33 @@ console.log(populationTotal);
 */
 
 
+function consume (a, b, cb){
+  return cb (a, b)
+}
+
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
+function add(num1, num2){
+  return num1 + num2;
+}
+
+function multiply (num1, num2){
+  return num1 * num2
+}
+
+function greeting(firstName, lastName){
+  return `Hello ${firstName} ${lastName}, nice to meet you!`
+}
+
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// console.log(consume(2, 2, add)); // 4
-// console.log(consume(10, 16, multiply)); // 160
-// console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2, 2, add)); // 4
+console.log(consume(10, 16, multiply)); // 160
+console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 
--- challenges/classes.js
+++ challenges/classes.js
@@ -32,3 +32,23 @@ console.log(cuboid1.volume()); // 100
 console.log(cuboid1.surfaceArea()); // 130
 
 // Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
+
+class CubeMaker extends CuboidMaker1 {
+    constructor(length, width, height){
+        super(length, width, height);
+    }
+
+    cubeVol(){
+    return this.length ** 3;
+    }
+
+    cubeSurfaceArea(){
+    return 6 * (this.length**2)
+    }
+
+    
+    
+}
+let cube = new CubeMaker(4, 4, 4);
+console.log(cube.cubeVol());
+console.log(cube.cubeSurfaceArea());
